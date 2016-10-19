7f6c3d8002a8b9bbb5735e9a20cc1531ad4ed696
lastone9182 <lastone9182@gmail.com>
2016-10-19 18:20:56 +0000

    tracking master

--- facetracking.py
+++ facetracking.py
@@ -10,7 +10,7 @@ import pdb
 face_cascade = cv2.CascadeClassifier('/var/www/flask/haarcascade_frontalface_default.xml')
 
 
-filename = 'mov_bbb.mp4'
+filename = 'gf_na.mp4'
 
 
 cap = cv2.VideoCapture(filename)
@@ -22,18 +22,14 @@ cap = cv2.VideoCapture(filename)
 global detect_face
 global trackzone
 global prev_gray
-global roi_gray
-
-global hist_snap
-
+global roi_hist
+global itertrack
 
 
-
-trackzone = np.array([], np.int32)
-roi_gray = np.zeros
-hist_snap = np.zeros
-
-
+trackzone = np.array([],dtype=np.int32) 
+roi_hist = np.zeros
+detect_face = False 
+itertrack = 0
 
 
 
@@ -70,65 +66,72 @@ while(1):
         ################### Detection
 
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-        
-
-
-
-
-
-
-
 
         if faces == tuple():
             detect_face = False
         else:
+            if trackzone.shape[0] == 0:
+                trackzone = faces
+
+
+        itertrack = 0
+
+        for x, y, w, h in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+
             detect_face = True
-
-
-
-
-
-
-
-
-        if detect_face:
-            #pdb.set_trace()
-            trackzone = faces
-
-
+            
         
+        for a, b in zip(trackzone, faces):
+            if ((np.abs(a[0]-b[0]) > 15) or (np.abs(a[1]-b[1]) > 15)) :
+                trackzone = np.vstack((trackzone, b))
+
+
 
 
 
 
         #################### Tracking
 
+        itertrack = 0
+        
         for x, y, w, h in trackzone:
-            
-            ################ Histogram
-            
-            if hist_snap != []:
-                hist_snap = cv2.calcHist([prev_gray[y:y+h, x:x+w]], [0], None, [180], [0,180])
-            
-            
-            
-            roi_hist = cv2.calcHist([roi_gray], [0], None, [180], [0,180])
-         
-            
-            
-            simval = cv2.compareHist(hist_snap, roi_hist, cv2.HISTCMP_CORREL)
-            print simval
 
-            if simval < 90.0:
-                detect_face = False
-                hist_snap = roi_hist
-                np.delete(trackzone, 0, 0)
-                continue
-            ####################### hist_snap 복수 얼굴 인식 시 문제 해결 요망
+            j = 0
+            itertrack = (itertrack % trackzone.shape[0])
+
+
+
+            for nx, ny, nw, nh in trackzone:
+                if j == itertrack:
+                    continue
+                if ((np.abs(nx-x) < 15) and (np.abs(ny-y) < 15)):
+                    trackzone = np.delete(trackzone, j, 0)
+
+                j = j + 1
+
+
+            itertrack = itertrack + 1
+
+
+
+
+
+        print 'd = %d t = %s' % (detect_face, tuple(trackzone))
+
+
+
+        
+        itertrack = 0
+        
+        for x, y, w, h in trackzone:
+            itertrack = (itertrack % trackzone.shape[0])
+            
+            
+            
+            #print itertrack
 
 
 
@@ -140,27 +143,64 @@ while(1):
             for j in range(h):
                 for i in range(w):
                     [ny, nx] = [y, x] + flow[y:y+h, x:x+w][j,i]
-            print nx, ny
+                    if nx < 0:
+                        nx = 0
+                    if ny < 0:
+                        ny = 0
+
+            
+            
+            
+            #print nx, ny
+            
+            
+            
+            
             nx = int(round(nx))
             ny = int(round(ny))
 
             
             cv2.rectangle(frame, (nx, ny), (nx+w, ny+h), (0,0,255), 2)
 
+            # 순서 중요
+            trackzone = np.vstack((trackzone, np.array([nx, ny, w, h], dtype=np.int32)))
+            trackzone = np.delete(trackzone, itertrack, 0)
+
+
+            ################ Histogram
+            #pdb.set_trace()
+        
+            prev_hist = cv2.calcHist([prev_gray[ny:ny+h, nx:nx+w]], [0], None, [180], [0,180])
+       
+            roi_hist = cv2.calcHist([gray[y:y+h, x:x+w]], [0], None, [180], [0,180])
+         
+
+
+                ## if compareHist(calcHist([query]) , roi_hist) < 90:
+                ##      continue
+         
+            
+            simval = cv2.compareHist(prev_hist, roi_hist, cv2.HISTCMP_CORREL)
+            
+            
+            
+            print simval
+            
+            
+            
+            
+            if simval < 0.75:
+                trackzone = np.delete(trackzone, itertrack, 0)
+
+
+
+            itertrack = itertrack + 1
 
 
 
 
-
-
-            ################ Trackzone Initialization
-            np.append(trackzone, [nx, ny, w, h])
-            np.delete(trackzone, 0, 0)
-
-
-
-        res = np.vstack((prev_frame, frame))
-        cv2.imshow('frame', res)
+        #res = np.vstack((frame))
+        cv2.imshow('frame', frame)
 
 
 
@@ -173,7 +213,7 @@ while(1):
     else:
         break
     
-    k = cv2.waitKey(25) & 0xFF
+    k = cv2.waitKey(80) & 0xFF
     if k == 27:
         break
 
